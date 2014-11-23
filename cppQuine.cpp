960bbc993fe036a5d4545ad21033b16796591e23
pcuste1 <pcuste1@umbc.edu>
2014-11-23 13:52:30 -0500

    Create cppQuine.cpp

---
+++ cppQuine.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char*argv[])
+{
+  char q = 34;
+  string codeBlock[] = {
+    "    ",
+    "#include <iostream>",
+    "#include <string>",
+    " ",
+    "using namespace std;",
+    " ",
+    "int main(int argc, char*argv[])",
+    "{",
+    "  char q = 34;",
+    "  string codeBlock[] = {",
+    "  };",
+    "  for(int i = 1; i < 10; i++)",
+    "    {",
+    "      cout << codeBlock[i] << endl;",
+    "    }",
+    "  for(int i = 0; i < 25; i++)",
+    "    {",
+    "      cout << codeBlock[0] + q + codeBlock[i] + q + ',' << endl;",
+    "    }",
+    "  cout << codeBlock[0] + q + codeBlock[25] + q + ',' << endl;",
+    "  for(int i = 10; i < 26; i++)",
+    "    {",
+    "      cout << codeBlock[i] << endl;",
+    "    }",
+    "  return 0;",
+        "}"
+  };
+  for(int i = 1; i < 10; i++)
+    {
+      cout << codeBlock[i] << endl;
+    }
+  for(int i = 0; i < 25; i++)
+    {
+      cout << codeBlock[0] + q + codeBlock[i] + q + ',' << endl;
+    }
+
+  cout << codeBlock[0] + q + codeBlock[25] + q + ',' << endl;
+  for(int i = 10; i < 26; i++)
+    {
+      cout << codeBlock[i] << endl;
+    }
+  return 0;
+}
